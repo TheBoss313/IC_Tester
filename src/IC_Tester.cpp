dfd1dda434d480ac6bdd5f40b88e7864c687b334
TheBoss313 <vlad.wpg@gmail.com>
2022-04-30 11:51:36 -0400

    update3

--- src/IC_Tester.cpp
+++ src/IC_Tester.cpp
@@ -1,6 +1,6 @@
 //
 // Created by vlad on 29/04/22.
-// GPIO 0-12 inclusive are "Data", GPIO 13 is controllable power
+// GPIO 2-13 inclusive are "Data", no controllable power
 #include "../include/IC_Tester.h"
 #include <Arduino.h>
 
@@ -13,11 +13,11 @@ IC_Tester::IC_Tester() : notList{TO_IC, FROM_IC, TO_IC, FROM_IC, TO_IC, FROM_IC,
                                 TO_IC, TO_IC, VCC},
                          norList{FROM_IC, TO_IC, TO_IC, FROM_IC, TO_IC, TO_IC, GND, TO_IC, TO_IC, FROM_IC, TO_IC, TO_IC,
                                  FROM_IC, VCC},
-                         pinList{0, 1, 2, 3, 4, 5, ARD_GND, 6, 7, 8, 9, 10, 11, ARD_VCC},
+                         pinList{2, 3, 4,5, 6, 7, ARD_GND, 8, 9, 10, 11, 12, 13, ARD_VCC},
                          andGates{{0,  1,  2},
                                   {3,  4,  5},
-                                  {7,  8,  9},
-                                  {10, 11, 12}},
+                                  {9, 8, 7},
+                                  {12, 11, 10}},
                          orGates{{0,  1,  2},
                                  {3,  4,  5},
                                  {7,  8,  9},
@@ -40,147 +40,165 @@ void IC_Tester::setPins(const int gateList[14]) {
             pinMode(pinList[i], OUTPUT);
         } else if (gateList[i] == FROM_IC) {
             pinMode(pinList[i], INPUT);
-        } else if (gateList[i] == VCC) {
-            pinMode(pinList[i], OUTPUT);
-        }
+        } //else if (gateList[i] == VCC) {
+            //pinMode(pinList[i], OUTPUT);
+        //}
     }
 }
 
 void IC_Tester::scan(const int gate) {
     bool test_res;
-    int gates[4][3];
     int list[14];
-    int cases[4][3];
+    // Power to VCC
+    // digitalWrite(ARD_VCC, HIGH);
     if (gate == OR) {
+        Serial.println("Scanning OR gate...");
         // case follows scheme [in, in, out]
         int cases[4][3] = {{LOW,  LOW,  LOW},
                            {LOW,  HIGH, HIGH},
                            {HIGH, LOW,  HIGH},
                            {HIGH, HIGH, HIGH}};
         setPins(orList);
-        // power to VCC
-        digitalWrite(ARD_VCC, HIGH);
-        for (unsigned int i = 0; i < sizeof(orGates); i++) {
-            for (unsigned int j = 0; j < sizeof(orGates[0]); j++) {
-                gates[i][j] = orGates[i][j];
-            }
-        }
-        for (unsigned int i = 0; i < sizeof(orList); i++) {
+        for (int i = 0; i < 14; i++) {
             list[i] = orList[i];
         }
-    } else if (gate == AND) {
+        int test_outputs[4][4];
+        for (int gateI = 0; gateI < 4; gateI++) {
+            int locGate[3];
+            for (int i = 0; i < 3; i++) {
+                locGate[i] = orGates[gateI][i];
+            }
+
+            for (int test = 0; test < 4; test++) {
+                digitalWrite(list[locGate[0]], cases[test][0]);
+                digitalWrite(list[locGate[1]], cases[test][1]);
+                test_res = digitalRead(list[locGate[2]]) == cases[test][2];
+                Serial.print(digitalRead(list[locGate[2]]));
+                Serial.print(" ");
+                Serial.print(cases[test][2]);
+                Serial.print(" ");
+                Serial.println(test_res);
+                test_outputs[gateI][test] = test_res;
+            }
+        }
+        for (int i=0; i < 4; i++) {
+            Serial.print("Gate #");
+            Serial.println(i + 1);
+            int counter = 1;
+            for (int j = 0; j < 4; j++) {
+                String out = "";
+                if (test_outputs[i][j]) { out = "PASS"; } else { out = "FAIL"; }
+                Serial.print("Condition ");
+                Serial.print(counter);
+                Serial.print(" ");
+                Serial.println(out);
+                counter++;
+            }
+        }
+    }
+    else if (gate == AND) {
+        Serial.println("Scanning AND gate...");
         // case follows scheme [in, in, out]
         int cases[4][3] = {{LOW,  LOW,  LOW},
                            {LOW,  HIGH, LOW},
                            {HIGH, LOW,  LOW},
                            {HIGH, HIGH, HIGH}};
         setPins(andList);
-        // power to VCC
-        digitalWrite(ARD_VCC, HIGH);
-        for (unsigned int i = 0; i < sizeof(andGates); i++) {
-            for (unsigned int j = 0; j < sizeof(andGates[0]); j++) {
-                gates[i][j] = andGates[i][j];
-            }
-        }
-        for (unsigned int i = 0; i < sizeof(andList); i++) {
+        for (int i = 0; i < 14; i++) {
             list[i] = andList[i];
         }
-    } else if (gate == NOR) {
+        int test_outputs[4][4];
+        for (int gateI = 0; gateI < 4; gateI++) {
+            int locGate[3];
+            for (int i = 0; i < 3; i++) {
+                locGate[i] = andGates[gateI][i];
+            }
+
+            for (int test = 0; test < 4; test++) {
+                    digitalWrite(list[locGate[0]], cases[test][0]);
+                    digitalWrite(list[locGate[1]], cases[test][1]);
+                    test_res = digitalRead(list[locGate[2]]) == cases[test][2];
+                Serial.print(digitalRead(list[locGate[2]]));
+                Serial.print(" ");
+                Serial.print(cases[test][2]);
+                Serial.print(" ");
+                Serial.println(test_res);
+                    test_outputs[gateI][test] = test_res;
+            }
+        }
+        for (int i=0; i < 4; i++) {
+            Serial.print("Gate #");
+            Serial.println(i + 1);
+            int counter = 1;
+            for (int j = 0; j < 4; j++) {
+                String out = "";
+                if (test_outputs[i][j]) { out = "PASS"; } else { out = "FAIL"; }
+                Serial.print("Condition ");
+                Serial.print(counter);
+                Serial.print(" ");
+                Serial.println(out);
+                counter++;
+            }
+        }
+    }
+    else if (gate == NOR) {
+        Serial.println("Scanning NOR gate...");
         // case follows scheme [in, in, out]
         int cases[4][3] = {{LOW,  LOW,  HIGH},
                            {LOW,  HIGH, LOW},
                            {HIGH, LOW,  LOW},
                            {HIGH, HIGH, LOW}};
         setPins(norList);
-        // power to VCC
-        digitalWrite(ARD_VCC, HIGH);
-        for (unsigned int i = 0; i < sizeof(norGates); i++) {
-            for (unsigned int j = 0; j < sizeof(norGates[0]); j++) {
-                gates[i][j] = norGates[i][j];
-            }
-        }
-        for (unsigned int i = 0; i < sizeof(norList); i++) {
+        for (int i = 0; i < 14; i++) {
             list[i] = norList[i];
         }
-    } else if (gate == NOT) {
-        // case follows scheme [in, in, out]
-        int cases[4][3] = {{LOW,  LOW,  LOW},
-                           {LOW,  HIGH, HIGH},
-                           {HIGH, LOW,  HIGH},
-                           {HIGH, HIGH, HIGH}};
-        setPins(notList);
-        // power to VCC
-        digitalWrite(ARD_VCC, HIGH);
-        for (unsigned int i = 0; i < sizeof(notGates); i++) {
-            for (unsigned int j = 0; j < sizeof(notGates[0]); j++) {
-                gates[i][j] = notGates[i][j];
-            }
-        }
-        for (unsigned int i = 0; i < sizeof(notList); i++) {
-            list[i] = notList[i];
-        }
-    }
-    if (gate == AND || gate == OR || gate == NOR) {
         int test_outputs[4][4];
         for (int gateI = 0; gateI < 4; gateI++) {
             int locGate[3];
             for (int i = 0; i < 3; i++) {
-                locGate[i] = gates[gateI][i];
+                locGate[i] = norGates[gateI][i];
             }
 
             for (int test = 0; test < 4; test++) {
-                if (list[locGate[0]] == TO_IC) {
-                    digitalWrite(list[locGate[0]], cases[test][0]);
-                }
-                if (list[locGate[1]] == TO_IC) {
-                    digitalWrite(list[locGate[0]], cases[test][1]);
-                }
-                if (list[locGate[2]] == TO_IC) {
-                    digitalWrite(list[locGate[0]], cases[test][2]);
-                }
-                if (list[locGate[0]] == FROM_IC) {
-                    test_res = digitalRead(list[locGate[0]]) == cases[test][0];
-                    test_outputs[gateI][test] = test_res;
-                }
-                if (list[locGate[1]] == FROM_IC) {
-                    test_res = digitalRead(list[locGate[0]]) == cases[test][1];
-                    test_outputs[gateI][test] = test_res;
-                }
-                if (list[locGate[2]] == FROM_IC) {
-                    test_res = digitalRead(list[locGate[0]]) == cases[test][2];
-                    test_outputs[gateI][test] = test_res;
-                }
-            }
-        }
-        for (unsigned int i = 0; i < sizeof(test_outputs); i++) {
-            Serial.print("Gate #");
-            Serial.println(i + 1);
-            for (unsigned int j = 0; j < sizeof(test_outputs[0]); j++) {
-                String out = "";
-                if (test_outputs[i][j]) { out = "PASS"; } else { out = "FAIL"; }
-                Serial.print("Condition ");
-                Serial.print(j + 1);
-                Serial.println(out);
-            }
-        }
-    } else if (gate == NOT) {
-        int test_outputs[6][2];
-        for (int gateI = 0; gateI < 6; gateI++) {
-            for (int test = 0; test < 6; test++) {
-                if (gates[gateI][0]) {}
-            }
-        }
-        for (unsigned int i = 0; i < sizeof(test_outputs); i++) {
-            Serial.print("Gate #");
-            Serial.println(i + 1);
-            for (unsigned int j = 0; j < sizeof(test_outputs[0]); j++) {
-                String out = "";
-                if (test_outputs[i][j]) { out = "PASS"; } else { out = "FAIL"; }
-                Serial.print("Condition ");
-                Serial.print(j + 1);
-                Serial.println(out);
+                digitalWrite(list[locGate[0]], cases[test][0]);
+                digitalWrite(list[locGate[1]], cases[test][1]);
+                test_res = digitalRead(list[locGate[2]]) == cases[test][2];
+                Serial.print(digitalRead(list[locGate[2]]));
+                Serial.print(" ");
+                Serial.print(cases[test][2]);
+                Serial.print(" ");
+                Serial.println(test_res);
+                test_outputs[gateI][test] = test_res;
             }
         }
     }
-    digitalWrite(ARD_VCC, LOW);
+    else if (gate == NOT) {
+        Serial.println("Scanning NOT gate...");
+        // case follows scheme [in, out]
+        int cases[2][2] = {{HIGH, LOW},
+                           {LOW,  HIGH}};
+        setPins(notList);
+        for (int i = 0; i < 14; i++) {
+            list[i] = notList[i];
+        }
+        int test_outputs[6][2];
+        for (int gateI = 0; gateI < 6; gateI++) {
+            int locGate[2];
+            for (int i = 0; i < 2; i++) {
+                locGate[i] = notGates[gateI][i];
+            }
+
+            for (int test = 0; test < 2; test++) {
+                digitalWrite(list[locGate[0]], cases[test][0]);
+                test_res = digitalRead(list[locGate[1]]) == cases[test][1];
+                Serial.print(digitalRead(list[locGate[1]]));
+                Serial.print(" ");
+                Serial.print(cases[test][1]);
+                Serial.print(" ");
+                Serial.println(test_res);
+                test_outputs[gateI][test] = test_res;
+            }
+        }
+    }
+    // digitalWrite(ARD_VCC, LOW);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@ IC_Tester icTest;
 int incomingByte = 0;
 bool bad_input = true;
 int gate;
+
 void setup() {
     Serial.begin(9600);
 }
@@ -17,31 +18,31 @@ void loop() {
         if (Serial.available() > 0) {
             // read the incoming byte:
             incomingByte = Serial.read();
-            Serial.print("Received: ");
-            Serial.println(incomingByte);
             switch (incomingByte) {
-                case 0:
+                case 48:
                     gate = NOR;
+                    bad_input = false;
                     break;
-                case 1:
+                case 49:
                     gate = NOT;
                     bad_input = false;
                     break;
-                case 2:
+                case 50:
                     gate = AND;
                     bad_input = false;
                     break;
-                case 3:
+                case 51:
                     gate = OR;
                     bad_input = false;
                     break;
                 default:
                     Serial.println("Invalid IC type. Try Again.");
                     Serial.println("Enter Gate type as number: \nLS02 - 0\nLS04 - 1\nLS08 - 2\nLS32 - 3");
-                    if (!bad_input) {
-                        icTest.scan(gate);
-                    }
             }
+            if (!bad_input) {
+                icTest.scan(gate);
+            }
+
         }
     }
 }
